303e03f8b843d7230c5f1e640ca1c958993016fd
Sam Saccone <samccone@gmail.com>
2015-09-09 22:38:09 -0700

    tab => space consistency

--- src/background.js
+++ src/background.js
@@ -14,65 +14,65 @@ var polymerPanelPorts = new Map();
 // listen for polymer devtools panel, and element-zones content
 // script connections
 chrome.extension.onConnect.addListener(function(port) {
-	if (port.name == 'polymer-panel') {
-		console.log('polymer-panel connected', port);
+  if (port.name == 'polymer-panel') {
+    console.log('polymer-panel connected', port);
 
-		port.onMessage.addListener(function(message, port) {
-			console.log('from polymer-panel', message, port);
-			if (!message.messageType) return;
-			var messageType = message.messageType;
-			if (messageType === 'tab-id') {
-				console.log('polymer-page tab-id', message.tabId);
-				// save the port to the polymer devtools panel
-				polymerPanelPorts.set(message.tabId, port);
+    port.onMessage.addListener(function(message, port) {
+      console.log('from polymer-panel', message, port);
+      if (!message.messageType) return;
+      var messageType = message.messageType;
+      if (messageType === 'tab-id') {
+        console.log('polymer-page tab-id', message.tabId);
+        // save the port to the polymer devtools panel
+        polymerPanelPorts.set(message.tabId, port);
 
-				port.postMessage({
-					messageType: 'handshake',
-				});
+        port.postMessage({
+          messageType: 'handshake',
+        });
 
-			} else if (messageType === 'get-element-stats' ||
-						messageType === 'clear-element-stats') {
-							console.log('messageType', messageType);
-				// relay to content script
-				var tabId = message.tabId;
-				var pagePort = pagePorts.get(tabId);
-				pagePort.postMessage({
-					messageType: messageType,
-				});
-			}
-		});
+      } else if (messageType === 'get-element-stats' ||
+            messageType === 'clear-element-stats') {
+        console.log('messageType', messageType);
+        // relay to content script
+        var tabId = message.tabId;
+        var pagePort = pagePorts.get(tabId);
+        pagePort.postMessage({
+          messageType: messageType,
+        });
+      }
+    });
 
-	} else if (port.name =='element-zones') {
-		console.log('element-zones.onConnect', port, port.sender.tab.id);
-		// save the port to the element-zones content script
-		pagePorts.set(port.sender.tab.id, port);
-		port.onMessage.addListener(function(message, port) {
-			if (message.messageType == 'element-stats') {
-				console.log('element-stats', message.data);
-				// post to devtools panel
-				var tabId = port.sender.tab.id;
-				var polymerPanelPort = polymerPanelPorts.get(port.sender.tab.id);
-				console.log('polymer panel port', tabId, polymerPanelPort);
-				console.assert(polymerPanelPort != null);
-				polymerPanelPort.postMessage({
-					messageType: 'element-stats',
-					data: message.data,
-				});
-			}
-		});
-	}
+  } else if (port.name =='element-zones') {
+    console.log('element-zones.onConnect', port, port.sender.tab.id);
+    // save the port to the element-zones content script
+    pagePorts.set(port.sender.tab.id, port);
+    port.onMessage.addListener(function(message, port) {
+      if (message.messageType == 'element-stats') {
+        console.log('element-stats', message.data);
+        // post to devtools panel
+        var tabId = port.sender.tab.id;
+        var polymerPanelPort = polymerPanelPorts.get(port.sender.tab.id);
+        console.log('polymer panel port', tabId, polymerPanelPort);
+        console.assert(polymerPanelPort != null);
+        polymerPanelPort.postMessage({
+          messageType: 'element-stats',
+          data: message.data,
+        });
+      }
+    });
+  }
 });
 
 // chrome.extension.onDisconnect.addListener(function(port) {
-// 	if (port.name == 'element-zones') {
-// 		console.log('element-zones.onDisconnect', port);
-// 		pagePorts.remove(port.sender.tab.id);
-// 	}
+//   if (port.name == 'element-zones') {
+//     console.log('element-zones.onDisconnect', port);
+//     pagePorts.remove(port.sender.tab.id);
+//   }
 // });
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	console.log('background chrome.runtime.onMessage', request, sender);
-	if (request.messageType == 'get_times') {
-		console.log('background get_times');
-	}
+  console.log('background chrome.runtime.onMessage', request, sender);
+  if (request.messageType == 'get_times') {
+    console.log('background get_times');
+  }
 });
